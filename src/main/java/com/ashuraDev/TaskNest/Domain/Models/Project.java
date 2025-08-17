5bf5e056a772e98417f80c6074ef9623e42e7a12
ashura <ashurasan04@gmail.com>
2025-08-16 22:13:51 -0500

    create SpringDataProjectRepository

--- src/main/java/com/ashuraDev/TaskNest/Domain/Models/Project.java
+++ src/main/java/com/ashuraDev/TaskNest/Domain/Models/Project.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 
-
+@AllArgsConstructor
 @Data
 public class Project {
     private  Long id;
@@ -21,19 +21,6 @@ public class Project {
     private LocalDate endDate;
     private List<User> assignedUsers;
 
-
-    public Project(Long id, String projectName, String description, String projectState, Long ownerId, Boolean active, LocalDate starDate, LocalDate endDate, List<User> assignedUsers) {
-        this.id = id;
-        this.projectName = projectName;
-        this.description = description;
-        this.projectState = projectState;
-        this.ownerId = ownerId;
-        this.active = active;
-        this.starDate = starDate;
-        this.endDate = endDate;
-        this.assignedUsers = assignedUsers;
-    }
-
     public void activate() {
         this.active = true;
     }
@@ -41,7 +28,7 @@ public class Project {
     public void deactivate() {
         this.active = false;
     }
-    //show project details
+
     public void updateDetails(String projectName,String description, String projectState, boolean active, LocalDate starDate, LocalDate expirationDate){
         this.projectName = projectName;
         this.description = description;
@@ -50,11 +37,11 @@ public class Project {
         this.starDate = starDate;
         this.endDate = expirationDate;
     }
-    //if the project are expired
+
     public boolean isOverDue(){
         return LocalDate.now().isAfter(endDate);
     }
-    //if the current date is before the project's start date
+
     public boolean isUpcoming(){
         return LocalDate.now().isBefore(starDate);
     }
@@ -77,77 +64,7 @@ public class Project {
         return List.copyOf(assignedUsers);
      }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getProjectState() {
-        return projectState;
-    }
-
-    public void setProjectState(String projectState) {
-        this.projectState = projectState;
-    }
-
-    public Long getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(Long ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public LocalDate getStarDate() {
-        return starDate;
-    }
-
-    public void setStarDate(LocalDate starDate) {
-        this.starDate = starDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public List<User> getAssignedUsers() {
-        return assignedUsers;
-    }
-
-    public void setAssignedUsers(List<User> assignedUsers) {
-        this.assignedUsers = assignedUsers;
-    }
 }
 
 
---
+++ src/main/java/com/ashuraDev/TaskNest/Infrastructure/Repository/SpringDataProjectRepository.java
@@ -0,0 +1,12 @@
+package com.ashuraDev.TaskNest.Infrastructure.Repository;
+
+import com.ashuraDev.TaskNest.Infrastructure.Entities.ProjectEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SpringDataProjectRepository extends JpaRepository<ProjectEntity, Long> {
+
+
+    List<ProjectEntity> findByOwnerId(Long ownerId);
+}
